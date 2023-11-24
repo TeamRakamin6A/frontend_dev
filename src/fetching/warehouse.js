643d5bcda513e646cf8b75fc8713eb5c66cac0ff
MaulanaRafinda99 <144241327+MaulanaRafinda99@users.noreply.github.com>
2023-11-24 20:45:44 +0700

    Add Page Warehouses

--- src/fetching/warehouse.js
+++ src/fetching/warehouse.js
@@ -1,1 +1,13 @@
+import { FaRegHospital } from "react-icons/fa";
 import instance from "../lib/axios";
+
+// Get All Order    <<<< 
+export async function getAllWarehouses(page, limit, nameFilter) {
+    try {
+        const response = await instance.get(`/warehouses?page=${page}&limit=${limit}&name=${nameFilter}`);
+        const data = response.data;
+        return data;    
+    } catch (error) {
+        throw new Error(error.response.data.message || 'Error Not Found');
+    }
+}
