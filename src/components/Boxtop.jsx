712e9f3651296af1bf5f8242774e9df45f06ddbb
unknown <fadhilahreygan@gmail.com>
2023-11-25 10:26:51 +0700

    fix ui

--- src/components/Boxtop.jsx
+++ src/components/Boxtop.jsx
@@ -1,22 +1,19 @@
 import React from "react";
-import { Box, Heading, Text } from "@chakra-ui/react";
+import { Box, Text } from "@chakra-ui/react";
+import { Breadcrumb, BreadcrumbItem, BreadcrumbLink } from "@chakra-ui/breadcrumb";
+import { ChevronRightIcon } from "@chakra-ui/icons";
 
 const CustomHeader = ({ title, subtitle }) => (
-    <Box
-        backgroundColor="white"
-        width="100%"
-        minHeight="100px"
-        display="flex"
-        justifyContent="flex-start"
-        alignItems="center"
-        paddingLeft="20px"
-    >
-        <Box>
-            <Heading size="md" mb={2}>
-                {title}
-            </Heading>
-            <Text>{subtitle}</Text>
-        </Box>
+    <Box w={'full'} bgColor={'#FFFFFF'} padding={'28px'} shadow={'lg'}>
+        <Text fontWeight={'extrabold'} fontSize={'22px'}>{title}</Text>
+        <Breadcrumb spacing='8px' color={'#AAAAAA'} separator={<ChevronRightIcon color='gray.500' />}>
+            <BreadcrumbItem>
+                <BreadcrumbLink href='#'>{title}</BreadcrumbLink>
+            </BreadcrumbItem>
+            <BreadcrumbItem>
+                <BreadcrumbLink href='#'>{subtitle}</BreadcrumbLink>
+            </BreadcrumbItem>
+        </Breadcrumb>
     </Box>
 );
 
