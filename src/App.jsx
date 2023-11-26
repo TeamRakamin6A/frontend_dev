8558105cb18f87bfd18eedcac8b6922c20f40b75
ranggakrisnaa <111489517+ranggakrisnaa@users.noreply.github.com>
2023-11-26 18:23:58 +0700

    add private route

--- src/App.jsx
+++ src/App.jsx
@@ -24,37 +24,38 @@ import CreateOrder from "./pages/Orders/AddOrder";
 import CreateWarehouse from "./pages/Warehouses/AddWarehouse";
 import AddItem from "./pages/Items/AddItem";
 import EditItem from "./pages/Items/EditItem";
+import PrivateRoute from "./components/PrivateRoute";
 
 function App() {
   return (
     <ChakraProvider>
       <Router>
         <Routes>
-          <Route path="/" element={<Dashboard />} />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
-          <Route path="/products" element={<Items />} />
-          <Route path="/add-products" element={<AddItem />} />
-          <Route path="/edit-products/:id" element={<EditItem />} />
-          <Route path="/products/:id" element={<ItemDetail />} />
-          <Route path="/categories" element={<Categories />} />
-          <Route path="/categories/:id" element={<CategoryDetail />} />
-          <Route path="/orders" element={<Orders />} />
-          <Route path="/orders/:id" element={<OrderDetail />} />
-          <Route path="/addorders" element={<CreateOrder />} />
-          <Route path="/customers" element={<Customers />} />
-          <Route path="/customers/:id" element={<CustomerDetail />} />
-          <Route path="/addcustomers" element={<AddCustomer />} />
-          <Route path="/suppliers" element={<Suppliers />} />
-          <Route path="/suppliers/:id" element={<SupplierDetail />} />
-          <Route path="/addsuppliers" element={<AddSupplier />} />
-          <Route path="/supplier-orders" element={<Supply_Orders />} />
-          <Route path="/add-supplier-orders" element={<Add_Supply_Orders />} />
-          <Route path="/supplier-orders/:id" element={<Supply_OrderDetail />} />
-          <Route path="/warehouses" element={<Warehouses />} />
-          <Route path="/warehouses/:id" element={<WarehouseDetail />} />
-          <Route path="/addwarehouse" element={<CreateWarehouse />}></Route>
-          <Route path="/supply_orders" element={< Supply_Orders/>}></Route>
+          <Route path="/" element={<PrivateRoute><Dashboard /></PrivateRoute>} />
+          <Route path="/products" element={<PrivateRoute><Items /></PrivateRoute>} />
+          <Route path="/add-products" element={<PrivateRoute><AddItem /></PrivateRoute>} />
+          <Route path="/edit-products/:id" element={<PrivateRoute><EditItem /></PrivateRoute>} />
+          <Route path="/products/:id" element={<PrivateRoute><ItemDetail /></PrivateRoute>} />
+          <Route path="/categories" element={<PrivateRoute><Categories /></PrivateRoute>} />
+          <Route path="/categories/:id" element={<PrivateRoute><CategoryDetail /></PrivateRoute>} />
+          <Route path="/orders" element={<PrivateRoute><Orders /></PrivateRoute>} />
+          <Route path="/orders/:id" element={<PrivateRoute><OrderDetail /></PrivateRoute>} />
+          <Route path="/addorders" element={<PrivateRoute><CreateOrder /></PrivateRoute>} />
+          <Route path="/customers" element={<PrivateRoute><Customers /></PrivateRoute>} />
+          <Route path="/customers/:id" element={<PrivateRoute><CustomerDetail /></PrivateRoute>} />
+          <Route path="/addcustomers" element={<PrivateRoute><AddCustomer /></PrivateRoute>} />
+          <Route path="/suppliers" element={<PrivateRoute><Suppliers /></PrivateRoute>} />
+          <Route path="/suppliers/:id" element={<PrivateRoute><SupplierDetail /></PrivateRoute>} />
+          <Route path="/addsuppliers" element={<PrivateRoute><AddSupplier /></PrivateRoute>} />
+          <Route path="/supplier-orders" element={<PrivateRoute><Supply_Orders /></PrivateRoute>} />
+          <Route path="/add-supplier-orders" element={<PrivateRoute><Add_Supply_Orders /></PrivateRoute>} />
+          <Route path="/supplier-orders/:id" element={<PrivateRoute><Supply_OrderDetail /></PrivateRoute>} />
+          <Route path="/warehouses" element={<PrivateRoute><Warehouses /></PrivateRoute>} />
+          <Route path="/warehouses/:id" element={<PrivateRoute><WarehouseDetail /></PrivateRoute>} />
+          <Route path="/addwarehouse" element={<PrivateRoute><CreateWarehouse /></PrivateRoute>} />
+          <Route path="/supply_orders" element={<PrivateRoute>< Supply_Orders /></PrivateRoute>} />
         </Routes>
       </Router>
     </ChakraProvider >
