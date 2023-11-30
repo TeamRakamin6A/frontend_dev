4c628a15e44dd6bf8cd33281e90f10e6e211481c
ranggakrisnaa <111489517+ranggakrisnaa@users.noreply.github.com>
2023-11-30 21:58:23 +0700

    fix ui register login page

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -7,14 +7,11 @@ import {
   Select,
   Text,
   useToast,
-  Center
+  Center,
+  Flex
 } from "@chakra-ui/react";
 import { registerUser } from "../fetching/user";
-import { useNavigate } from "react-router-dom";
-
-const hexaColor = {
-  colorSubmit: '#44AADA'
-}
+import { Link, useNavigate } from "react-router-dom";
 
 const Register = () => {
   const [name, setName] = useState("");
@@ -61,73 +58,74 @@ const Register = () => {
   };
 
   return (
-    <div className="container" style={{ justifyContent: 'center' }}>
-      <Center>
-        <Box w="full" py={9} px={'200'} mx="150" mt={2}>
-          <Box borderWidth="1px" borderRadius="lg" p={10}>
-            <img src="../Stock Wise.png" width={250} height={250} alt="center" style={{ marginLeft: 150 }} />
-            <form>
-              {error && (
-                <Box color="red.500" mb={8}>
-                  {error}
-                </Box>
+    <Box className="container" display={'flex'} h={'100vh'} justifyContent={'center'} alignItems={'center'}>
+      <Box py={9} px={'200'} mx="150" mt={2}>
+        <Flex flexDirection={'column'} justifyContent={'center'} alignItems={'center'} border={'2px solid #CCC9C9'} borderRadius="lg" p={10}>
+          <img src="../Stock Wise.png" width={320} height={320} />
+          <form>
+            {error && (
+              <Box color="red.500" mb={8}>
+                {error}
+              </Box>
+            )}
+
+            <FormControl isRequired mt={8}>
+              <Input w={'480px'} h={"55px"} border={'2px solid #CCC9C9'} type="name" name="name" placeholder="Name" onChange={(e) => setName(e.target.value)} />
+            </FormControl>
+
+            <FormControl isRequired mt={4} onChange={(e) => setEmail(e.target.value)}>
+              <Input
+                type="email"
+                name="email"
+                placeholder="Email"
+                w={'480px'} h={"55px"} border={'2px solid #CCC9C9'}
+              />
+            </FormControl>
+
+            <FormControl isRequired mt={4} >
+              <Input w={'480px'} h={"55px"} border={'2px solid #CCC9C9'} type="name" name="username" placeholder="Username" onChange={(e) => setUsername(e.target.value)} />
+            </FormControl>
+
+            <FormControl isRequired mt={4}>
+              <Input
+                type="password"
+                placeholder="Password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                w={'480px'} h={"55px"} border={'2px solid #CCC9C9'}
+              />
+            </FormControl>
+
+            <FormControl isRequired mt={4}>
+              <Input
+                type="password"
+                placeholder="Confirm Password"
+                value={confirmPassword}
+                onChange={(e) => setConfirmPassword(e.target.value)}
+                w={'480px'} h={"55px"} border={'2px solid #CCC9C9'}
+              />
+              {password !== confirmPassword && (
+                <Text fontSize="xs" color="red.500">
+                  The password does not match.
+                </Text>
               )}
+            </FormControl>
 
-              <FormControl isRequired mt={8}>
-                <Input type="name" name="name" placeholder="Name" onChange={(e) => setName(e.target.value)} />
-              </FormControl>
+            <FormControl isRequired mt={4}>
+              <Select placeholder="Select Role" onChange={(e) => setRole(e.target.value)} w={'480px'} h={"55px"} border={'2px solid #CCC9C9'}>
+                <option value="admin">Admin</option>
+                <option value="superadmin">Super Admin</option>
+              </Select>
+            </FormControl>
 
-              <FormControl isRequired mt={4} onChange={(e) => setEmail(e.target.value)}>
-                <Input
-                  type="email"
-                  name="email"
-                  placeholder="Email"
-                />
-              </FormControl>
-
-              <FormControl isRequired mt={4} >
-                <Input type="name" name="username" placeholder="Username" onChange={(e) => setUsername(e.target.value)} />
-              </FormControl>
-
-              <FormControl isRequired mt={4}>
-                <Input
-                  type="password"
-                  placeholder="Password"
-                  value={password}
-                  onChange={(e) => setPassword(e.target.value)}
-                />
-              </FormControl>
-
-              <FormControl isRequired mt={4}>
-                <Input
-                  type="password"
-                  placeholder="Confirm Password"
-                  value={confirmPassword}
-                  onChange={(e) => setConfirmPassword(e.target.value)}
-                />
-                {password !== confirmPassword && (
-                  <Text fontSize="xs" color="red.500">
-                    The password does not match.
-                  </Text>
-                )}
-              </FormControl>
-
-              <FormControl isRequired mt={4}>
-                <Select placeholder="Select Role" onChange={(e) => setRole(e.target.value)}>
-                  <option value="admin">Admin</option>
-                  <option value="superadmin">Super Admin</option>
-                </Select>
-              </FormControl>
-
-              <Button mt={8} color={hexaColor.colorSubmit} type="button" onClick={handleSubmit}>
-                Register
-              </Button>
-            </form>
-          </Box>
-        </Box>
-      </Center>
-
-    </div>
+            <Button mt={8} w={'full'} h={"55px"} color={'blue'} type="button" onClick={handleSubmit}>
+              Register
+            </Button>
+          </form>
+          <Text mt={'20px'}>Have an account? <Link to={"/login"}><Box display={'inline'} color={'blue'} _hover={{ textDecoration: 'underline' }}>Login</Box></Link></Text>
+        </Flex>
+      </Box>
+    </Box>
   );
 };
 
