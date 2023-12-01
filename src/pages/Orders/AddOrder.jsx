aeef5e44bf5a0d82b13117c0fa8c0aade062f236
MaulanaRafinda99 <144241327+MaulanaRafinda99@users.noreply.github.com>
2023-12-01 23:01:43 +0700

    Fix Orders (#15)
    * Fix Orders
    
    * Fix Component Orders

--- src/pages/Orders/AddOrder.jsx
+++ src/pages/Orders/AddOrder.jsx
@@ -1,189 +1,340 @@
-import { useState } from 'react';
+import React, { useState, useEffect, useRef } from "react";
+import { Link } from "react-router-dom";
 import {
-    Box,
-    Container,
-    Heading,
-    Text,
     Button,
     Input,
-    Flex,
+    Select,
     useToast,
+    Box,
+    Flex,
+    Heading,
+    SimpleGrid,
+    useDisclosure,
+    Modal,
+    ModalOverlay,
+    ModalContent,
+    ModalHeader,
+    ModalFooter,
+    ModalBody,
+    ModalCloseButton,
     FormControl,
     FormLabel,
-} from '@chakra-ui/react';
-import { createOrder } from '../../fetching/order';
+    Table,
+    Thead,
+    Tbody,
+    Tfoot,
+    Tr,
+    Th,
+    Td,
+    TableCaption,
+    TableContainer,
+    Text
+} from "@chakra-ui/react";
+import Navbar from "../../components/Navbar";
+import {
+    createOrder
+} from "../../fetching/order";
+import { getAllWarehouses } from "../../fetching/warehouse";
+import { getAllItems } from "../../fetching/item";
+import { getAllCustomers } from "../../fetching/customer";
+import CustomHeader from "../../components/Boxtop";
+import Loading from "../../components/Loading";
+import convertPrice from "../../lib/convertPrice";
 import { useNavigate } from 'react-router-dom';
-import Navbar from '../../components/Navbar';
 
 const CreateOrder = () => {
-    const [formData, setFormData] = useState({
-        invoice: '',
-        total_price: '',
-        warehouse_id: '',
-        customer_id: '',
-        status: '',
-        item_id: '',
-        quantity: '',
-        price_item: ''
-    });
-
     const toast = useToast();
+
+    const [warehouses, setWarehouses] = useState([]);
+    const [customers, setCustomers] = useState([]);
+    const [items, setItems] = useState([]);
+    const { isOpen, onOpen, onClose } = useDisclosure()
+    const [itemList, setItemList] = useState([])
+    const [loading, setLoading] = useState(false)
+    const selectedItemRef = useRef({})
+    const newItemQuantityRef = useRef(0)
+
+    // Form Submit
+    const [customerId, setCustomerId] = useState(0);
+    const [warehouseId, setWarehouseId] = useState(0);
+    const [status, setStatus] = useState("pending");
+    const [totalPrice, setTotalPrice] = useState(0);
+
     const navigate = useNavigate();
 
-    const handleFormChange = (e) => {
-        setFormData({
-            ...formData,
-            [e.target.name]: e.target.value,
-        });
-    };
+    useEffect(() => {
+        setLoading(true)
+        fetchAll();
+    }, []);
 
-    const handleFormSubmit = async () => {
-
-        if (!formData.invoice || !formData.total_price || !formData.warehouse_id || !formData.customer_id || !formData.status || !formData.item_id || !formData.quantity || !formData.price_item) {
-            toast({
-                title: 'All fields are required.',
-                status: 'error',
-                duration: 3000,
-                isClosable: true,
-            });
-            return;
-        }
-
+    const fetchAll = async () => {
         try {
-            await createOrder(
-                formData.invoice,
-                formData.total_price,
-                formData.warehouse_id,
-                formData.customer_id,
-                formData.status,
-                formData.item_id,
-                formData.quantity,
-                formData.price_item
-            );
+            const responseWarehouses = await getAllWarehouses();
+            setWarehouses(responseWarehouses.data);
+            const responseCustomers= await getAllCustomers(1, 100, "");
+            setCustomers(responseCustomers.items);
+            const responseItems = await getAllItems(1, 100, "", []);
+            setItems(responseItems.data.items);
+            setLoading(false)
+        } catch (err) {
+            console.log(err)
+        }
+    }
 
+    const handleCreateOrder = async () => {
+        try {
+
+            const itemPayload = itemList.map((currentElement) => {
+                return {
+                    id: currentElement.item.id,
+                    quantity: currentElement.quantity,
+                    price_item: +currentElement.item.price
+                }
+            })
+            const payload = {
+                invoice: +1,
+                total_price: getTotalPrice(),
+                customer_id: customerId,
+                warehouse_id: warehouseId,
+                status: status,
+                items: itemPayload
+            }
+
+            await createOrder(payload)
+            navigate("/orders")
             toast({
-                title: 'Order added successfully.',
-                status: 'success',
+                title: "Success",
+                description: "Order created successfully",
+                status: "success",
                 duration: 3000,
                 isClosable: true,
             });
-
-            navigate('/orders');
-        } catch (error) {
-            console.error('Error Orders:', error.message);
+        } catch (err) {
             toast({
-                title: 'Error Orders.',
-                status: 'error',
+                title: "Error",
+                description: "Failed to create order",
+                status: "error",
                 duration: 3000,
                 isClosable: true,
             });
         }
-    };
+    }
+
+    const addNewItem = async () => {
+        const foundItem = items.find((e) => e.id === +selectedItemRef.current.value)
+        try {
+            let itemObject = {
+                item: foundItem,
+                quantity: +newItemQuantityRef.current.value,
+            }
+            const updatedItems = [...itemList]
+            // find existing item
+            if(itemList.length !== 0) {
+                let existingId = itemList.findIndex((e) => e.item.id === foundItem.id)
+                if(existingId !== -1) {
+                    // update item
+                    updatedItems[existingId] = {...updatedItems[existingId], quantity: +newItemQuantityRef.current.value}
+                    setItemList(updatedItems)
+                } else {
+                    // add new item
+                    setItemList([...itemList, itemObject])
+                }
+            } else {
+                // Create new item
+                setItemList([...itemList, itemObject])
+            }
+            setTotalPrice(getTotalPrice())
+            onClose();
+        } catch (err) {
+            console.log(err);
+        }
+    }
+
+    if (loading) {
+        return <Loading />
+    }
+
+    const getTotalPrice = () => {
+
+        const sum = itemList.reduce((accumulator, currentValue) => {
+            return accumulator + (+currentValue.item.price * +currentValue.quantity)
+        }, 0)
+
+        return sum;
+    }
+
+    const changeWarehouse = (id) => {
+        setWarehouseId(id),
+            setItemList([])
+    }
+
+    const ItemTable = () => {
+        return (
+            <>
+                <Table mb={30}>
+                    <Thead>
+                        <Tr>
+                            <Th>No</Th>
+                            <Th>Product</Th>
+                            <Th>Price</Th>
+                            <Th>Quantity</Th>
+                            <Th>Total Price</Th>
+                        </Tr>
+                    </Thead>
+                    <Tbody>
+                        {itemList?.map((el, idx) => (
+                            <Tr key={idx}>
+                                <Td>{idx + 1}</Td>
+                                <Td>{el.item.title}</Td>
+                                <Td>{convertPrice(el.item.price)}</Td>
+                                <Td>{el.quantity}</Td>
+                                <Td>{convertPrice(+el.item.price * +el.quantity)}</Td>
+                            </Tr>
+                        ))}
+                    </Tbody>
+                </Table>
+                <Box>
+                    <Text align={"right"}>Total Price {convertPrice(getTotalPrice())}</Text>
+                </Box>
+            </>
+        )
+    }
+
+    const findWarehouseById = () => {
+        return warehouses.find((w) => w.id === +warehouseId)
+    }
+
+    const ComponentItem = () => {
+        return (
+            <>
+                <Button colorScheme="teal" onClick={onOpen} mb={10}>Add Item</Button>
+                <Modal isOpen={isOpen} onClose={onClose}>
+                    <ModalOverlay />
+                    <ModalContent>
+                        <ModalHeader>Add New Item</ModalHeader>
+                        <ModalCloseButton />
+                        <ModalBody>
+                            <FormControl>
+                                <FormLabel>Item</FormLabel>
+                                <Select ref={selectedItemRef} placeholder='Select option' mb={3}>
+                                    {
+                                        findWarehouseById().Items?.map((product) =>
+                                        (
+                                            <option value={product.id} key={product.id}>{product.title}</option>
+                                        )
+                                        )
+                                    }
+                                </Select>
+                                <FormLabel>Quantity</FormLabel>
+                                <Input ref={newItemQuantityRef} placeholder='Quantity' />
+                            </FormControl>
+                        </ModalBody>
+
+                        <ModalFooter>
+                            <Button colorScheme='teal' onClick={addNewItem} mr={4}>Submit</Button>
+                            <Button colorScheme='blue' mr={3} onClick={onClose}>
+                                Close
+                            </Button>
+                        </ModalFooter>
+                    </ModalContent>
+                </Modal>
+            </>
+        )
+    }
 
     return (
-        <>
+        <div style={{ backgroundColor: "#f8f8f8", minHeight: "100vh" }}>
             <Navbar />
-            <Box bg="gray.200" minH="100vh" pb="5">
-                <Container maxW="" mb="5" bg="white" p="4" boxShadow="md">
-                    <Heading as="h1" fontSize="xl">
-                        Orders List
-                    </Heading>
-                    <Text fontSize="sm" color="gray.500">
-                        Order {'>'} Add Order
-                    </Text>
-                </Container>
+            <CustomHeader title="Orders" subtitle="Add Orders" />
+            <Box bg="white" m="40px" minHeight={"fit-content"}>
+                <Heading ml="60px" pt="40px" pb="20px" fontSize={'22px'}>
+                    Add Order
+                </Heading>
+                <Box display="flex">
+                    <Box flex="1" mx="60px" pt="20px">
+                        <SimpleGrid columns={2} spacing={10} marginBottom={30}>
+                            <Box style={{ fontSize: "14px", fontWeight: "bold", }}>
+                                Customer
+                                <Select
+                                    name="customer_id"
+                                    fontSize="14px"
+                                    height="40px"
+                                    onChange={(e) => setCustomerId(+(e.target.value))}
+                                    required
+                                >
+                                    <option value="">
+                                        Select a customer
+                                    </option>
+                                    {customers.map((customer) => (
+                                        <option key={customer.id} value={customer.id}>
+                                            {customer.name}
+                                        </option>
+                                    ))}
+                                </Select>
+                            </Box>
+                            <Box style={{ fontSize: "14px", fontWeight: "bold", }}>
+                                Warehouse
+                                <Select
+                                    name="warehouse_id"
+                                    fontSize="14px"
+                                    height="40px"
+                                    value={warehouseId}
+                                    onChange={(e) => changeWarehouse(+(e.target.value))}
+                                    required
+                                >
+                                    <option value="">
+                                        Select a warehouse
+                                    </option>
+                                    {warehouses.map((warehouse) => (
+                                        <option key={warehouse.id} value={warehouse.id}>
+                                            {warehouse.title}
+                                        </option>
+                                    ))}
+                                </Select>
+                            </Box>
+                            <Box style={{ fontSize: "14px", fontWeight: "bold", }}>
+                                Status
+                                <Select
+                                    name="status"
+                                    fontSize="14px"
+                                    height="40px"
+                                    value={status}
+                                    onChange={(e) => setStatus(e.target.value)}
+                                    required
+                                >
+                                    <option value="Pending">Pending</option>
+                                    <option value="Success">Success</option>
+                                </Select>
+                            </Box>
+                        </SimpleGrid>
+                        <Box>
+                            {warehouseId != 0 ? <ComponentItem /> : ''}
 
-                <Container maxW="145ch" bg="white" p="4" borderRadius="md" boxShadow="md">
-                    <Flex direction="column" m="5">
-                        <FormControl mb="4" isRequired>
-                            <FormLabel>Invoice</FormLabel>
-                            <Input
-                                name="invoice"
-                                placeholder="Invoice"
-                                value={formData.invoice}
-                                onChange={handleFormChange}
-                            />
-                        </FormControl>
+                            {itemList.length !== 0 ? <ItemTable /> : ""}
+                            <Flex justifyContent="flex-start" >
+                                <Button
+                                    type="button"
 
-                        <FormControl mb="4" isRequired>
-                            <FormLabel>Total Price</FormLabel>
-                            <Input
-                                name="total_price"
-                                placeholder="Total Price"
-                                value={formData.total_price}
-                                onChange={handleFormChange}
-                            />
-                        </FormControl>
-
-                        <FormControl mb="4" isRequired>
-                            <FormLabel>Warehouse ID</FormLabel>
-                            <Input
-                                name="warehouse_id"
-                                placeholder="Warehouse ID"
-                                value={formData.warehouse_id}
-                                onChange={handleFormChange}
-                            />
-                        </FormControl>
-
-                        <FormControl mb="4" isRequired>
-                            <FormLabel>Customers ID</FormLabel>
-                            <Input
-                                name="customer_id"
-                                placeholder="Customer ID"
-                                value={formData.customer_id}
-                                onChange={handleFormChange}
-                            />
-                        </FormControl>
-
-                        <FormControl mb="4" isRequired>
-                            <FormLabel>Status</FormLabel>
-                            <Input
-                                name="status"
-                                placeholder="Status Order"
-                                value={formData.status}
-                                onChange={handleFormChange}
-                            />
-                        </FormControl>
-
-                        <FormControl mb="4" isRequired>
-                            <FormLabel>Item ID</FormLabel>
-                            <Input
-                                name="item_id"
-                                placeholder="Item ID"
-                                value={formData.item_id}
-                                onChange={handleFormChange}
-                            />
-                        </FormControl>
-
-                        <FormControl mb="4" isRequired>
-                            <FormLabel>Quantity</FormLabel>
-                            <Input
-                                name="quantity"
-                                placeholder="Quantity"
-                                value={formData.quantity}
-                                onChange={handleFormChange}
-                            />
-                        </FormControl>
-
-                        <FormControl mb="4" isRequired>
-                            <FormLabel>Price Item</FormLabel>
-                            <Input
-                                name="price_item"
-                                placeholder="Item Price"
-                                value={formData.price_item}
-                                onChange={handleFormChange}
-                            />
-                        </FormControl>
-
-                        <Button colorScheme="blue" onClick={handleFormSubmit}>
-                            Add Order
-                        </Button>
-                    </Flex>
-                </Container>
+                                    colorScheme="messenger"
+                                    fontSize="md"
+                                    mr={5}
+                                    p={7}
+                                    onClick={handleCreateOrder}
+                                >
+                                    Add Orders
+                                </Button>
+                                <Link to="/orders">
+                                    <Button variant="outline" colorScheme="blue" p={7}>
+                                        Back to List Orders
+                                    </Button>
+                                </Link>
+                            </Flex>
+                        </Box>
+                    </Box>
+                </Box>
             </Box>
-        </>
+        </div >
 
     );
 };
-
 export default CreateOrder;
--- src/pages/Orders/OrderDetail.jsx
+++ src/pages/Orders/OrderDetail.jsx
@@ -1,5 +1,203 @@
+import React, { useState, useEffect } from "react";
+import { useParams, Link } from "react-router-dom";
+import {
+  Table,
+  Thead,
+  Tbody,
+  Tr,
+  Th,
+  Td,
+  IconButton,
+  Button,
+  Heading,
+  Select,
+  Flex,
+  Box,
+} from "@chakra-ui/react";
+import { EditIcon, CheckIcon, CloseIcon } from "@chakra-ui/icons";
+
+import {
+  getOrderById,
+  updateOrder,
+} from "../../fetching/order";
+import { getCustomerById } from "../../fetching/customer";
+import { getWarehouseById } from "../../fetching/warehouse";
+import Navbar from "../../components/Navbar";
+import CustomHeader from "../../components/Boxtop";
+import Loading from "../../components/Loading";
+import convertPrice from "../../lib/convertPrice";
+
 const OrderDetail = () => {
-  return <div>OrderDetail</div>;
+  const { id } = useParams();
+  const [order, setOrder] = useState({});
+  const [customer, setCustomer] = useState({});
+  const [warehouse, setWarehouse] = useState({});
+  const [isEditing, setIsEditing] = useState(false);
+  const [editedStatus, setEditedStatus] = useState("");
+  const [loading, setLoading] = useState(false)
+
+  useEffect(() => {
+    const fetchOrderDetail = async () => {
+      try {
+        const response = await getOrderById(id);
+        setOrder(response.data);
+
+        const customerResponse = await getCustomerById(response.data.customer_id);
+        setCustomer(customerResponse.data);
+        const warehouseResponse = await getWarehouseById(response.data.warehouse_id);
+        setWarehouse(warehouseResponse);
+        setLoading(false)
+      } catch (error) {
+        console.error("Error fetching order detail:", error.message);
+      }
+    };
+    setLoading(true)
+    fetchOrderDetail();
+  }, []);
+
+  const handleEditClick = () => {
+    setIsEditing(true);
+    setEditedStatus(order.status);
+  };
+
+  const handleCancelEdit = () => {
+    setIsEditing(false);
+  };
+
+  const handleSaveEdit = async () => {
+    try {
+      await updateOrder(id, editedStatus);
+
+      const response = await getOrderById(id);
+      setOrder(response.data);
+
+      setIsEditing(false);
+    } catch (error) {
+      console.error("Error updating order:", error.message);
+    }
+  };
+
+  if (loading) {
+    return <Loading />
+  }
+  const statusOptions = ["Pending", "Success"];
+
+  return (
+    <div style={{ backgroundColor: "#f8f8f8", minHeight: "100vh" }}>
+      <Navbar />
+      <CustomHeader title="Orders" subtitle="Orders Detail" />
+      <Box
+        backgroundColor="white"
+        margin="20px 20px"
+        padding="20px"
+        borderRadius="8px"
+        boxShadow="0 4px 8px rgba(0,0,0,0.1)"
+        display="flex"
+        flexDirection="column"
+      >
+        <Box mx={"40px"} pt={"20px"}>
+          <Heading fontSize={'22px'}>Orders Detail</Heading>
+          <Table
+            variant="simple"
+            colorScheme="gray"
+            my={"20px"}
+            maxWidth="full"
+            borderWidth="1px"
+            borderColor="gray.200"
+          >
+            <Thead>
+              <Tr fontWeight="bold">
+                <Th width="" fontWeight="bold" fontSize="14px" textTransform="none" textColor={"black"}>Invoice</Th>
+                <Th width="" fontWeight="bold" fontSize="14px" textTransform="none" textColor={"black"}>Total Price</Th>
+                <Th width="" fontWeight="bold" fontSize="14px" textTransform="none" textColor={"black"}>Customer</Th>
+                <Th width="" fontWeight="bold" fontSize="14px" textTransform="none" textColor={"black"}>Warehouse</Th>
+                <Th width="" fontWeight="bold" fontSize="14px" textTransform="none" textColor={"black"}>Status</Th>
+                <Th width="" fontWeight="bold" fontSize="14px" textTransform="none" textColor={"black"}>Action</Th>
+              </Tr>
+            </Thead>
+            <Tbody>
+              <Tr>
+                <Td textColor={"gray.600"}>{order.invoice}</Td>
+                <Td textColor={"gray.600"}>{order.total_price}</Td>
+                <Td textColor={"gray.600"}>{customer.name}</Td>
+                <Td textColor={"gray.600"}>{warehouse.title}</Td>
+                <Td textColor={"gray.600"}>
+                  {isEditing ? (
+                    <Select
+                      value={editedStatus}
+                      onChange={(e) => setEditedStatus(e.target.value)}
+                    >
+                      {statusOptions.map((option) => (
+                        <option key={option} value={option}>
+                          {option}
+                        </option>
+                      ))}
+                    </Select>
+                  ) : (
+                    order.status
+                  )}
+                </Td>
+                <Td fontSize="14px" textColor={"gray.600"}>
+                  {isEditing ? (
+                    <>
+                      <IconButton
+                        icon={<CheckIcon />}
+                        size="xs"
+                        onClick={handleSaveEdit}
+                        colorScheme="green"
+                      />
+                      <IconButton
+                        icon={<CloseIcon />}
+                        size="xs"
+                        onClick={handleCancelEdit}
+                        colorScheme="red"
+                      />
+                    </>
+                  ) : (
+                    <IconButton
+                      icon={<EditIcon />}
+                      size="lg"
+                      onClick={handleEditClick}
+                      colorScheme="blue"
+                    />
+                  )}
+                </Td>
+              </Tr>
+            </Tbody>
+          </Table>
+
+          <Table mb={30}>
+            <Thead>
+              <Tr>
+                <Th>No</Th>
+                <Th>Product</Th>
+                <Th>Price</Th>
+                <Th>Quantity</Th>
+                <Th>Total Price</Th>
+              </Tr>
+            </Thead>
+            <Tbody>
+              {order.Items?.map((el, idx) => (
+                <Tr key={idx}>
+                  <Td>{idx + 1}</Td>
+                  <Td>{el.title}</Td>
+                  <Td>{convertPrice(el.price)}</Td>
+                  <Td>{el.Order_Item.quantity}</Td>
+                  <Td>{convertPrice(+el.price * +el.Order_Item.quantity)}</Td>
+                </Tr>
+              ))}
+            </Tbody>
+          </Table>
+
+          <Flex justifyContent="flex-end" ml={16}>
+            <Button as={Link} to="/orders" variant="outline" colorScheme="blue" p={5}>
+              Back to List Orders
+            </Button>
+          </Flex>
+        </Box>
+      </Box>
+    </div>
+  );
 };
 
 export default OrderDetail;
